414d66d92cae5d13f8ef4243d3a8679d2c717eb6
Cricky Cicchetti <cricky.cicchetti@gmail.com>
2018-10-02 00:53:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function calculatesFarePrice(startStreet, endStreet){
     return (((Math.abs(startStreet-endStreet)*264)-400)*2)/100
   }
   else if (Math.abs(startStreet-endStreet)*264 > 2000){
-    return 25
+    return 25;
   }
   else if (Math.abs(startStreet-endStreet)*264 > 2500){
     return 'cannot travel that far'
