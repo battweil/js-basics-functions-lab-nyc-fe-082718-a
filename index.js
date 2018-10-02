433119c8bd003e627379f0cd96c65012162f80cf
Cricky Cicchetti <cricky.cicchetti@gmail.com>
2018-10-02 00:35:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(currentLocation){
+  return Math.abs(42-currentLocation)
+}
+
+function distanceFromHqInFeet(currentLocation){
+  return Math.abs(42-currentLocation)*264
+}
+
+function distanceTravelledInFeet(startStreet, endStreet){
+  return Math.abs(startStreet-endStreet)*264
+}
+
+function calculatesFarePrice(start, destination){
+  if (Math.abs(startStreet-endStreet)*264)
+}
